5880716afed266e8b3149f71a1c6229eda0a62b9
Md. Shariful Islam <shariful@bjitgroup.com>
2018-10-20 20:43:37 +0900

    POST PUT DELETE Method added

--- mvc_2.0.5/src/main/java/com/example/mvc_205/controller/HelloController.java
+++ mvc_2.0.5/src/main/java/com/example/mvc_205/controller/HelloController.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.mvc_205.model.*;
 
 @RestController
 public class HelloController {
+	
+	ArrayList<Order> orderList = new ArrayList<Order>();
 	
 	@GetMapping("/order")
 	public List<Order> getOrder()
@@ -25,29 +29,26 @@ public class HelloController {
 	
 
 	
-	@DeleteMapping("/order")
-	public List<Order> deleteOrder()
-	{
-		ArrayList<Order> orderList = new ArrayList<Order>();
-		orderList.add(new Order(3434L, "New Order", 1));
-		
-		return orderList;
+	@DeleteMapping("/order/{id}")
+	public String deleteOrder(@PathVariable Long id)
+	{		
+		return "deleted with id :"+id;
 	}
 	
-	@PutMapping("/order")
-	public List<Order> updateOrder()
+	@PutMapping("/order/{id}")
+	public List<Order> updateOrder(@RequestBody Order order, @PathVariable Long id)
 	{
-		ArrayList<Order> orderList = new ArrayList<Order>();
-		orderList.add(new Order(3434L, "New Order", 1));
+		order.setId(id);
+		orderList.add(order);
 		
 		return orderList;
 	}
 	
 	@PostMapping("/order")
-	public List<Order> PostOrder()
+	public List<Order> PostOrder(@RequestBody Order newOrder)
 	{
 		ArrayList<Order> orderList = new ArrayList<Order>();
-		orderList.add(new Order(3434L, "New Order", 1));
+		orderList.add(newOrder);
 		
 		return orderList;
 	}
